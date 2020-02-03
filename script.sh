815cff20e9f73d2687285ef5829bf464483e8bd3
Igor Moura <imp2@cin.ufpe.br>
2020-02-03 16:57:41 -0300

    works without video

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-echo -e 'hello\n\n\n\n\n\nhello'
 /usr/bin/ffmpeg -i rtmp://localhost/mytv/$1 \
-                -b:a 64k -c:v libx264 -preset fast -profile:v baseline -vsync cfr -s 1280x720 -b:v 2500K -bufsize 2500k -f flv \
-                -max_muxing_queue_size 1024 rtmp://localhost/dash/$1 2>>/var/log/ffmpeg-$name.log
+                -b:a 64k -c:v libx264 -c:a aac -preset fast -profile:v baseline -vsync cfr -s 1280x720 -b:v 2500K -bufsize 2500k -f flv \
+                -max_muxing_queue_size 1024 rtmp://localhost/hls/$1 2>>/var/log/ffmpeg-$1.log
