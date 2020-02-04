8904653ac00016050b76f3e7eaf77cda31470b5e
Igor Moura <imp2@cin.ufpe.br>
2020-02-04 12:41:29 -0300

    trying out dash instead of hls

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 /usr/bin/ffmpeg -i rtmp://localhost/mytv/$1 \
                 -c:v libx264 -c:a aac -preset ultrafast -s 1280x720 -b:v 1000K -bufsize 1000k -f flv \
-                -max_muxing_queue_size 1024 rtmp://localhost/hls/$1 2>>/var/log/ffmpeg-$1.log
+                -max_muxing_queue_size 1024 rtmp://localhost/dash/$1 2>>/var/log/ffmpeg-$1.log
