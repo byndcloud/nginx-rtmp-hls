7b3a50143aa9eb1da43f66ed7acff70503c632ee
Igor Moura <imp2@cin.ufpe.br>
2020-02-04 15:37:31 -0300

    add new low res

--- script.sh
+++ script.sh
@@ -3,5 +3,7 @@
                 -c:v libx264 -c:a aac -preset ultrafast -tune zerolatency -async 1 -s 1280x720 -b:v 1000K -bufsize 1000k -f flv \
                 -max_muxing_queue_size 1024 rtmp://localhost/hls/$1_hi \
                 -c:v libx264 -c:a aac -preset ultrafast -tune zerolatency -async 1 -s 640x360 -b:v 500K -bufsize 500k -f flv \
+                -max_muxing_queue_size 1024 rtmp://localhost/hls/$1_med \
+                -c:v libx264 -c:a aac -preset ultrafast -tune zerolatency -async 1 -s 320x180 -b:v 250K -bufsize 250k -f flv \
                 -max_muxing_queue_size 1024 rtmp://localhost/hls/$1_low \
                 2>>/var/log/ffmpeg-$1.log
